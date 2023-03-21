fcd3424bcaaa06fed2c6f9aa6c5c9d64e19cdd7c
timayyy <timilehin65@gmail.com>
2023-03-21 19:19:44 +0100

    Added site title and description

--- src/pages/waitlist.tsx
+++ src/pages/waitlist.tsx
@@ -1,10 +1,21 @@
 import { FormSection } from "@/components/form-section";
 import { Navabar } from "@/components/navbar";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const WaitListPage: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Nkuzi</title>
+        <meta
+          name="description"
+          content="Nkuzi helps international students lead a decent life by connecting them to affordable accommodation, flexible jobs, low interest finance and like-minded communities."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>One-stop platform for international students</title>
+      </Head>
       <Navabar />
       <div className="min-h-screen bg-[#E7CFFF] py-20">
         <FormSection />
